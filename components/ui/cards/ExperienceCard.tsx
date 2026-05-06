badaa20e71db8e668a3933dc8ad64f2e906c012a
xyryc <amdanikzz@gmail.com>
2026-05-07 02:42:56 +0600

    refactor: experience section

--- components/ui/cards/ExperienceCard.tsx
+++ components/ui/cards/ExperienceCard.tsx
@@ -23,49 +23,52 @@ const ExperienceCard = ({
 }: ExperienceCardProps) => {
   const { t } = useTranslation();
   return (
-    <View
-      className={`p-2.5  ${className} ${isCurrent ? " border rounded-2xl mt-12" : "border border-[#0000000D] rounded-2xl"}`}>
+    <View className={`${className} ${isCurrent ? "pt-9" : ""}`}>
       {isCurrent && (
-        <View className="absolute -top-9 right-2">
+        <View className="absolute top-0.5 right-3 z-10">
           <Image
             source={require("@/assets/images/experience.svg")}
             contentFit="contain"
             style={{ height: 30, width: 150 }}
           />
-          <Text className="-top-6 text-center font-proximanova-semibold text-sm text-primary dark:text-dark-primary">
+          <Text className="-mt-6 text-center font-proximanova-semibold text-sm text-primary dark:text-dark-primary">
             {t("user.profile.userProfile.currentWorking")}
           </Text>
         </View>
       )}
-      <View className="flex-row justify-between ">
-        <View className="flex-row items-center gap-3">
-          <View>
-            <Image
-              source={companyLogo}
-              contentFit="cover"
-              style={{ height: 50, width: 50, borderRadius: 999 }}
-            />
+      <View
+        className={`p-2.5 rounded-2xl ${isCurrent ? "border border-gray-400" : "border border-gray-300"}`}
+      >
+        <View className="flex-row justify-between ">
+          <View className="flex-row items-center gap-3">
+            <View>
+              <Image
+                source={companyLogo}
+                contentFit="cover"
+                style={{ height: 50, width: 50, borderRadius: 999 }}
+              />
+            </View>
+            <View>
+              <Text className="font-proximanova-semibold text-sm text-primary dark:text-dark-primary">
+                {companyName || t("user.profile.userProfile.company")}
+              </Text>
+              <Text className="font-proximanova-regular text-sm text-secondary dark:text-dark-secondary">
+                {position ? `${t("user.profile.userProfile.workingAs")} ${position}` : t("user.profile.userProfile.roleNotSpecified")}
+              </Text>
+            </View>
           </View>
-          <View>
-            <Text className="font-proximanova-semibold text-sm text-primary dark:text-dark-primary">
-              {companyName || t("user.profile.userProfile.company")}
-            </Text>
-            <Text className="font-proximanova-regular text-sm text-secondary dark:text-dark-secondary">
-              {position ? `${t("user.profile.userProfile.workingAs")} ${position}` : t("user.profile.userProfile.roleNotSpecified")}
-            </Text>
-          </View>
-        </View>
 
-        {/* hiruu logo */}
-        {isVerified && (
-          <View>
-            <Image
-              source={require("@/assets/images/hiruu-logo.svg")}
-              contentFit="contain"
-              style={{ height: 14, width: 38 }}
-            />
-          </View>
-        )}
+          {/* hiruu logo */}
+          {isVerified && (
+            <View>
+              <Image
+                source={require("@/assets/images/hiruu-logo.svg")}
+                contentFit="contain"
+                style={{ height: 14, width: 38 }}
+              />
+            </View>
+          )}
+        </View>
       </View>
     </View>
   );
